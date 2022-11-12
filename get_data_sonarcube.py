612432d3f4e1b2159f923a6e37fd4c5b704d7bfd
sonia-goyal <goyal.25@iitj.ac.in>
2022-11-12 20:49:24 +0530

    Update get data sonarcube

--- get_data_sonarcube.py
+++ get_data_sonarcube.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def get_sonarcube_data():
     df = pd.DataFrame()
-    for i in range(1, 201):
+    for i in range(1, 210):
         print(f"====== Interation {i} ======")
         url = "https://sonarcloud.io/api/components/search_projects?boostNewProjects=false&facets=ncloc%2Clanguages&p=" + \
               str(i) + "&ps=50&f=analysisDate&s=analysisDate&asc=false"
@@ -13,16 +13,18 @@ def get_sonarcube_data():
         headers = {}
 
         response = requests.request("GET", url, headers=headers, data=payload)
-        response = response.json()
-
-        sub_df = pd.DataFrame(response['components'])
-        df = pd.concat([df, sub_df])
+        if response.status_code == 400:
+            break
+        else:
+            response = response.json()
+            sub_df = pd.DataFrame(response['components'])
+            df = pd.concat([df, sub_df])
 
     # print(df.head())
-
-    df.to_csv('sonarcube_data.csv', index=False)
+    df = df.sort_values(by='organization')
+    df.to_csv('sonarcube_data_1.csv', index=False)
 
 
 if __name__ == '__main__':
-    get_sonarcube_data
+    get_sonarcube_data()
 
